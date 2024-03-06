796a4faa9c8c533a2f26e9b9bb0eccf5877e3936
MateoRoman <105375095+MateoRoman@users.noreply.github.com>
2024-03-06 08:14:56 -0500

    Update app.module.ts
    correción app.module.ts

--- evaluacion/src/app.module.ts
+++ evaluacion/src/app.module.ts
@@ -8,7 +8,7 @@ import { TareasModule } from './tareas/tarea.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://nando:nando@cluster0.wko5jxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), AulaModule, TareasModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://nando:nando@cluster0.wko5jxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), AulaModule],
   controllers: [AppController],
   providers: [AppService],
 })
